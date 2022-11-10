829450cc48efe21f52d4a073379b4c5fabe0e739
Crown Royal <glenn6@umbc.edu>
2022-11-10 01:21:19 +0000

    afds

--- walk.py
+++ walk.py
@@ -1,13 +1,12 @@
 import os
 import platform
 
-if platform.system() == "Windows":
-	xmls = os.getcwd() + '\\xmls'
-else:
-	xmls = os.getcwd() + '/xmls'
+# if platform.system() == "Windows":
+# 	xmls = os.getcwd() + '\\xmls'
+# else:
 
+xmls = os.getcwd() + '/xmls'
 print(xmls)
 for root, dirs, files in os.walk(xmls):
 	for i in files:
-		pass
 		print(os.path.join(root, i))
